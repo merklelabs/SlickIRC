e2db135b5287c2ab29b715c7120a9f2906d00dc5
Ian Macalinao <me@ian.pw>
2015-11-29 11:44:09 -0600

    Implement new server button

--- app/components/ServerList.js
+++ app/components/ServerList.js
@@ -1,13 +1,30 @@
 import Radium from 'radium'
 import React, { Component, PropTypes } from 'react'
 import styles from './ServerList.styles.js'
+import { updatePath } from 'redux-simple-router'
+import { connect } from 'react-redux'
 
 @Radium
 class ServerList extends Component {
 
   render() {
     return <div style={[ styles.base ]}>
+      <AddButton />
     </div>
+  }
+
+}
+
+@connect()
+@Radium
+class AddButton extends Component {
+
+  render() {
+    const { dispatch } = this.props
+
+    return <button style={ styles.addButton } onClick={() => dispatch(updatePath('/servers/new'))}>
+      <i className="fa fa-plus fa-2x"></i>
+    </button>
   }
 
 }
--- app/components/ServerList.styles.js
+++ app/components/ServerList.styles.js
@@ -3,5 +3,13 @@ export default {
     width: '60px',
     height: '100vh',
     backgroundColor: '#ccc'
+  },
+
+  addButton: {
+    width: '40px',
+    height: '40px',
+    margin: '10px',
+    position: 'absolute',
+    bottom: 0
   }
 }
